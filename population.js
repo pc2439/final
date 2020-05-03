41571637156bf22abde459011ee76991ab13c28e
pc2439 <61560870+pc2439@users.noreply.github.com>
2020-05-04 02:20:54 +0800

    uploading files

---
+++ population.js
@@ -0,0 +1,23 @@
+const counters = document.querySelectorAll('.counter');
+
+// Set speed, the lower the slower
+const speed = 300; 
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+		const inc = target / speed;
+
+
+		// Check if target is reached
+		if (count < target) {
+			counter.innerText = count + inc;
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
